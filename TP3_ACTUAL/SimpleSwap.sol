ed00b3b375b6e7cb1d09ff46c5e19aeab9cc2429
transfors <transfor2008@gmail.com>
2025-06-18 04:23:25 -0300

    "agregados"

--- TP3_ACTUAL/SimpleSwap.sol
+++ TP3_ACTUAL/SimpleSwap.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.20;
+pragma solidity ^0.8.27;
 
 // Importar contratos de OpenZeppelin para funcionalidades seguras
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol"; // Interfaz estándar para tokens ERC-20
